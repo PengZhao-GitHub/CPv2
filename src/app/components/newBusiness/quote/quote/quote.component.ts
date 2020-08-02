c5f3e63d2ed27f3086eeb4b7a1322d415bf3a577
PengZhao-GitHub <pengzhao2000jp@yahoo.co.jp>
2020-08-02 21:55:56 +0900

    quote

--- src/app/components/newBusiness/quote/quote/quote.component.ts
+++ src/app/components/newBusiness/quote/quote/quote.component.ts
@@ -29,6 +29,16 @@ export class QuoteComponent implements OnInit {
   selectedProductID: string;
   selectedProduct: string;
 
+  selectedCoverages = [];
+
+  premium; 
+
+
+  ageRange = Array.from(Array(61), (_, i) => i + 20); //Array.from(Array(100).keys());
+
+  age: number = 30;
+  sex: string = "F";
+
 
   constructor(
     private routeParam: ActivatedRoute,
@@ -40,14 +50,17 @@ export class QuoteComponent implements OnInit {
 
   ngOnInit() {
 
-    console.log('test this.routeParam.paramMap', this.routeParam.paramMap);
+    /* close Index and Footer section to give a clean screen which focus on quote only*/
+    document.getElementById('main-links').style.display = 'none';
+    document.getElementById('main-footer').style.display = 'none';
+    document.getElementById('gotop').style.marginBottom = '60px';
 
     /* Manage state among angular components*/
     /************************************** */
     this.state$ = this.routeParam.paramMap
       .pipe(map(() => {
-        let product = window.history.state; /*get state object*/
-        console.log('inside of state:', product);
+          let product = window.history.state; /*get state object*/
+          console.log('inside of state:', product);
 
           /* get CMS system id and product name */
           this.selectedInsurer = product.insurer.name;
@@ -59,7 +72,7 @@ export class QuoteComponent implements OnInit {
           this.selectedProduct = product.name;
           /* end of getting CMS system id and product name */
 
-          // Call PAS API
+          // Call PAS API to get the product structure from PAS system
           this.productService.getProduct(product['product_id']).subscribe(result => {
             if (result) {
               console.log('PAS API', result);
@@ -67,22 +80,37 @@ export class QuoteComponent implements OnInit {
               if (this.productDetail) {
                 this.coverages = this.productDetail['coverages'];
                 console.log('quote-init', this.coverages);
+
+                //Populate the defaul selection - select all.  
+                /************************************** */
+                this.coverages.forEach(cov => {
+                  console.log(cov.coverageCode, cov.limitOption);
+                  let limitOption = cov.limitOption;
+                  let defaultLimit = limitOption[limitOption.length - 1];
+                  this.selectedCoverages.push({ coverageCode: cov.coverageCode, option: defaultLimit});
+                });
+            
+                console.log(this.selectedCoverages);
+                //Call API to get the default premium
+                this.callQuoteAPI(); 
               }
 
             } else {
               this.productDetail = null;
             }
           })
-
         return product;
       }));
 
+    console.log('checkpoint!');
+    
     //To solve the Error: ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked. Previous value: 'undefined: undefined'. Current value: 'undefined: 8'.
+    /************************************** */
     this.cd.detectChanges();
-
+    
     //Go to the top of the screen
+    /************************************** */
     window.scroll(0, 0);
-
 
   }
 
@@ -90,4 +118,99 @@ export class QuoteComponent implements OnInit {
     this.router.navigate(['/products', this.selectedInsurerID, this.selectedInsurer, this.selectedProductLineID, this.selectedProductLine, this.selectedInsurerUrl]);
   }
 
+
+  onSelectCoverage(event){
+    console.log(event.target.value);
+    console.log(event.target.checked);
+    console.log(event.target.name);
+    console.log(event.target.id);
+
+    console.log(this.selectedCoverages);
+
+    var coverageCode = event.target.value;
+    var coverageChecked = event.target.checked;
+
+    let coverageDetail = document.getElementById(coverageCode); //get the form element
+    console.log('Select the form', coverageDetail);
+    
+    if (!coverageChecked) {
+
+      coverageDetail.style.display = "none";
+      //Remove the coverage 
+      this.selectedCoverages = this.selectedCoverages.filter(t => t.coverageCode != coverageCode);
+      console.log(this.selectedCoverages);
+    
+    } else {
+      coverageDetail.style.display ="flex";
+
+      /* Use vanila java script to manage the form - Radio values*/
+      var form = document.getElementById(coverageCode); //get the form element
+      var optionListValue = form[coverageCode].value;  //get the radio box list of inside of the form by using the radio name
+      console.log("get radio value", optionListValue);
+
+      this.selectedCoverages.push({ coverageCode: coverageCode, option: optionListValue });
+      console.log(this.selectedCoverages);
+
+      
+
+    }
+
+    //Call API
+    this.callQuoteAPI();
+  
+  }
+
+  onSelectOption(event) {
+    console.log(event.target.value);
+    console.log(event.target.checked);
+    console.log(event.target.name);
+
+    var coverageCode = event.target.name;
+    var selectedOption = event.target.value;
+
+    //if the selected coverageCode exit, update option
+    this.selectedCoverages.forEach(t => {
+      if (t.coverageCode === coverageCode) {
+        t.option = selectedOption;
+      }
+    })
+
+    console.log(this.selectedCoverages);
+
+    //Call API
+    this.callQuoteAPI();
+
+  }
+
+  onAgeChange(event){
+   this.callQuoteAPI();
+
+  }
+
+  onGenderChange(event){
+    this.callQuoteAPI();
+  }
+
+  callQuoteAPI(){
+    if (this.selectedCoverages.length >= 0) {
+      var customer = {
+        sex: this.sex,
+        age: this.age
+      }
+      console.log('cusotmer:', customer);
+      console.log('Selected Coverages:', this.selectedCoverages);
+      this.productService.getQuote({ customer: customer, coverages: this.selectedCoverages }).subscribe(quote => {
+        this.premium = quote['result'].totalPremium;
+      });
+    }
+  }
+
+
+  ngOnDestroy(){
+    //window.alert('Will distroy this component');
+    /* Restore the links and footer*/
+    document.getElementById('main-links').style.display = '';
+    document.getElementById('main-footer').style.display = '';
+    document.getElementById('gotop').style.margin = '';
+  }
 }
